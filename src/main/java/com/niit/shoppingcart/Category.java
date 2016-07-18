233984acc2a122cdae3ab36599567b6970a7f0c9
Sneha <Sneha@LAPTOP-447VI6QQ>
2016-07-18 15:52:13 +0530

    spring mvc

---
+++ src/main/java/com/niit/shoppingcart/Category.java
@@ -0,0 +1,30 @@
+package com.niit.shoppingcart;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Category {
+	private String id;
+	private String name;
+	
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	private String description;
+	
+	
+
+}
